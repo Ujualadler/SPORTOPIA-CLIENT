a94412bf113c33bb76981fa3c32957f60ba914d3
ujual <ujualk2000@gmail.com>
2023-09-06 20:48:46 +0530

    capital

--- src/Pages/User/HomePage.jsx
+++ src/Pages/User/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../../Components/User/NavbarUser/NavbarUser'
-import UserHome from '../../Components/User/Home/userHome'
-import Carousal from '../../Components/Utilities/Carousal'
+import UserHome from '../../Components/User/Home/UserHome'
 import Footer from '../../Components/User/Footer/Footer'
 
 function HomePage() {
