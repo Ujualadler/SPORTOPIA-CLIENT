01c4c26d03e3deea1b4e4a91fc7683fd3a8efdfa
ujual <ujualk2000@gmail.com>
2023-09-10 21:46:11 +0530

    change url

--- src/Pages/User/GetTurfs.jsx
+++ src/Pages/User/GetTurfs.jsx
@@ -7,7 +7,7 @@ function GetTurfs() {
   return (
     <>
       <Navbar />
-      <div className="pt-4  m-1 pb-4 bg-cover bg-center min-h-screen bg-opacity-90 bg-gray-800 bg-[url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdqy8XnHVMpJGZiYBnzoFO0cYJjgAu7ndi4Q&usqp=CAU)]">
+      <div className="pt-4  m-1 pb-4 bg-cover bg-center min-h-screen bg-gray-800 ">
         <Booking />
       </div>
       <Footer />
